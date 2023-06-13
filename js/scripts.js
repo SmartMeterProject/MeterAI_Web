db94f8c49f49506871a97cdcab9c9c0769cce152
Hamdi1698 <121123023+Hamdi1698@users.noreply.github.com>
2023-06-13 19:57:44 +0300

    Code

---
+++ js/scripts.js
@@ -0,0 +1,167 @@
+
+
+(function($) {
+    "use strict"; 
+	
+	/* Preloader */
+	$(window).on('load', function() {
+		var preloaderFadeOutTime = 500;
+		function hidePreloader() {
+			var preloader = $('.spinner-wrapper');
+			setTimeout(function() {
+				preloader.fadeOut(preloaderFadeOutTime);
+			}, 500);
+		}
+		hidePreloader();
+	});
+
+	
+	/* Navbar Scripts */
+	// jQuery to collapse the navbar on scroll
+    $(window).on('scroll load', function() {
+		if ($(".navbar").offset().top > 60) {
+			$(".fixed-top").addClass("top-nav-collapse");
+		} else {
+			$(".fixed-top").removeClass("top-nav-collapse");
+		}
+    });
+
+	// jQuery for page scrolling feature - requires jQuery Easing plugin
+	$(function() {
+		$(document).on('click', 'a.page-scroll', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 600, 'easeInOutExpo');
+			event.preventDefault();
+		});
+	});
+
+    // closes the responsive menu on menu item click
+    $(".navbar-nav li a").on("click", function(event) {
+    if (!$(this).parent().hasClass('dropdown'))
+        $(".navbar-collapse").collapse('hide');
+    });
+
+
+    /* Image Slider - Swiper */
+    var imageSlider = new Swiper('.image-slider', {
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: false
+		},
+        loop: false,
+        navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+        spaceBetween: 30,
+        slidesPerView: 5,
+		breakpoints: {
+            // when window is <= 516px
+            516: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            },
+            // when window is <= 767px
+            767: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            // when window is <= 991px
+            991: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            },
+            // when window is <= 1199px
+            1199: {
+                slidesPerView: 4,
+                spaceBetween: 30
+            },
+        }
+    });
+
+
+    /* Image Lightbox - Magnific Popup */
+	$('.popup-link').magnificPopup({
+		removalDelay: 300,
+		type: 'image',
+		callbacks: {
+			beforeOpen: function() {
+				this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure ' + this.st.el.attr('data-effect'));
+			},
+			beforeClose: function() {
+				$('.mfp-figure').addClass('fadeOut');
+			}
+		},
+		gallery:{
+			enabled:true //enable gallery mode
+		}
+    });
+    
+
+    
+
+    /* Details Lightbox - Magnific Popup */
+	$('.popup-with-move-anim').magnificPopup({
+		type: 'inline',
+		fixedContentPos: false, /* keep it false to avoid html tag shift with margin-right: 17px */
+		fixedBgPos: true,
+		overflowY: 'auto',
+		closeBtnInside: true,
+		preloader: false,
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-slide-bottom'
+    });
+    
+
+  
+    
+    
+    /* Move Form Fields Label When User Types */
+    // for input and textarea fields
+    $("input, textarea").keyup(function(){
+		if ($(this).val() != '') {
+			$(this).addClass('notEmpty');
+		} else {
+			$(this).removeClass('notEmpty');
+		}
+    });
+
+
+    /* Privacy Form */
+    $("#privacyForm").validator().on("submit", function(event) {
+    	if (event.isDefaultPrevented()) {
+            // handle the invalid form...
+            pformError();
+            psubmitMSG(false, "Please fill all fields!");
+        } else {
+            // everything looks good!
+            event.preventDefault();
+            psubmitForm();
+        }
+    });
+
+   
+
+
+    /* Back To Top Button */
+    // create the back to top button
+    $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
+    var amountScrolled = 700;
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > amountScrolled) {
+            $('a.back-to-top').fadeIn('500');
+        } else {
+            $('a.back-to-top').fadeOut('500');
+        }
+    });
+
+
+	/* Removes Long Focus On Buttons */
+	$(".button, a, button").mouseup(function() {
+		$(this).blur();
+	});
+
+})(jQuery);
